5bb19b3f8e60e1d26941e91dcd4485b0d78045f1
TieableCookie2 <TiemenBlankestijn@gmail.com>
2025-05-16 22:03:44 +0200

    feat: added neuralNetwork function
    neuralNetwork function in neuralNetwork.py uses w1.csv and w2.csv and an input vector to give out (V2,V3) vector, the weights at each node in the network

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -42,8 +42,34 @@ def delOutActiv(x):
 #input a single vector (x_1, ... x_N)
 #output is tuple of (V_2, V_3) where V_2 is the 600-dimensional vector of the values at the hidden layer nodes and V_3 is the 10-dimensional prob. vector
 def neuralNetwork(input):
-    print(input)
+    #load files
+    w1 = np.loadtxt('w1.csv', delimiter=',')
+    w2 = np.loadtxt('w2.csv', delimiter=',')    
 
+    #turn input string into vector
+    Values1 = np.fromstring(input, sep=',')
+
+    #add a 1 to the front of the vector, !remember the first entry of the vector is the label!
+    Values1[0] = 1
+    
+    #Multiply Values1 by w1 matrix to get Values2, proof: trivial
+    Values2 = np.dot(Values1, w1)
+
+    #put activation function on Values2
+    activ2 = [activ(i) for i in Values2]
+
+    #prepend 1 to activated Hidden layer values vector
+    activ2 = np.insert(activ2, 0, 1)
+    
+    #get the output values from activ2 and the w2 weights
+    Values3 = np.dot(activ2,w2)
+
+    #commented out
+    #Output = [outActiv(i) for i in Values3]
+    #print(Output)
+
+    return (Values2, Values3)
+    
 
 if __name__ == "__main__":
     with open("single_example.csv", "r") as csvfile:
