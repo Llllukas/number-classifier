0268d21b5536d6d38cc7b6b7f7d7b4155abbb9c8
TieableCookie2 <TiemenBlankestijn@gmail.com>
2025-05-16 22:06:04 +0200

    fix: changed the probability interval in randomcsv
    because of law of the law of large numbers and the multiplied RVs having positive expected value there was never a negative value in the entire matrix, making the interval (-1,1) fixes this

--- randomcsv.py
+++ randomcsv.py
@@ -6,11 +6,11 @@ import numpy as np
 
 def generateWeights():
     #generate w1 (weights between input and hidden layer), unif(-1,2) distribution
-    w1 = np.random.uniform(low=-1, high=2, size=(Feature + 1, Hidden))
+    w1 = np.random.uniform(low=-1, high=1, size=(Feature + 1, Hidden))
     #save to w1.csv with 5 decimal places
     np.savetxt("w1.csv", w1, delimiter=",", fmt="%.5f")
 
-    w2 = np.random.uniform(low=-1, high=2, size=(Hidden + 1, 10))
+    w2 = np.random.uniform(low=-1, high=1, size=(Hidden + 1, 10))
     np.savetxt("w2.csv", w2, delimiter=",", fmt="%.5f")
 
 
