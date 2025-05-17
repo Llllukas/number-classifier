908b0931db8aa01b479a2732864dbee2a3b9bf3c
TieableCookie2 <TiemenBlankestijn@gmail.com>
2025-05-17 16:55:28 +0200

    fix: randomcsv.py now generates matrices with growth rate 1

--- randomcsv.py
+++ randomcsv.py
@@ -5,12 +5,12 @@ import numpy as np
 
 
 def generateWeights():
-    #generate w1 (weights between input and hidden layer), unif(-1,1) distribution
-    w1 = np.random.uniform(low=-1, high=1, size=(Feature + 1, Hidden))
+    #generate w1 (weights between input and hidden layer), distribution adjusted as to have growth rate of 1
+    w1 = np.random.uniform(low=-2/Feature, high=2/Feature, size=(Feature + 1, Hidden))
     #save to w1.csv with 5 decimal places
     np.savetxt("w1.csv", w1, delimiter=",", fmt="%.5f")
 
-    w2 = np.random.uniform(low=-1, high=1, size=(Hidden + 1, 10))
+    w2 = np.random.uniform(low=-4/Hidden, high=4/Hidden, size=(Hidden + 1, 10))
     np.savetxt("w2.csv", w2, delimiter=",", fmt="%.5f")
 
 
